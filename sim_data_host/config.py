983bf1bb7bd737ae5d815b7b358902c6494b5d1a
wegar2 <awegrzyn17@gmail.com>
2026-01-21 17:47:34 +0100

    introduced GbmSim config

--- sim_data_host/config.py
+++ sim_data_host/config.py
@@ -1,2 +1,10 @@
 from pydantic import BaseModel
 
+
+class GbmSimulationConfig(BaseModel):
+    start_value: float = 100
+    mu_daily: float = 0.005
+    sigma_daily: float = 0.02
+    value_seed: int = 654_321
+    lambda_arrival_time: float = 5
+    arrival_time_seed: int = 123_456
--- sim_data_host/simulation.py
+++ sim_data_host/simulation.py
@@ -6,33 +6,27 @@ import numpy as np
 
 from sim_data_host.data_update import DataUpdate
 from sim_data_host.constants import SECONDS_PER_DAY
+from sim_data_host.config import GbmSimulationConfig
 
 DataQueue: asyncio.Queue = asyncio.Queue()
 
 
-async def gbm_simulation(
-    start_value: float = 100,
-    mu_daily: float = 0.005,
-    sigma_daily: float = 0.02,
-    value_seed: int = 654_321,
-    lambda_arrival_time: float = 5,
-    arrival_time_seed: int = 123_456
-):
-    value: float = start_value
+async def gbm_simulation(config: GbmSimulationConfig):
+    value: float = config.start_value
 
-    value_rng = np.random.default_rng(value_seed)
-    arrival_time_rng = np.random.default_rng(seed=arrival_time_seed)
+    value_rng = np.random.default_rng(config.value_seed)
+    arrival_time_rng = np.random.default_rng(seed=config.arrival_time_seed)
 
     while True:
 
         d_time = float(
-            arrival_time_rng.exponential(scale=arrival_time_seed, size=1)[0])
+            arrival_time_rng.exponential(scale=config.lambda_arrival_time, size=1)[0])
         await asyncio.sleep(d_time)
 
         value = (
             value +
-            mu_daily * (d_time / SECONDS_PER_DAY) +
-            sigma_daily *
+            config.mu_daily * (d_time / SECONDS_PER_DAY) +
+            config.sigma_daily *
             math.sqrt(d_time / SECONDS_PER_DAY) *
             float(value_rng.normal(size=1)[0])
         )
