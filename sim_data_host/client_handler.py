332119af0771c87a729b6e3168bea4703b3cec82
wegar2 <awegrzyn17@gmail.com>
2026-01-21 21:55:15 +0100

    added implementation of client_handler

--- sim_data_host/client_handler.py
+++ sim_data_host/client_handler.py
@@ -1,4 +1,4 @@
-import asyncio
+from asyncio import StreamReader, StreamWriter
 import logging
 
 from sim_data_host.broadcaster import clients
@@ -6,10 +6,22 @@ from sim_data_host.broadcaster import clients
 logger = logging.getLogger(__name__)
 
 
-async def client_handler(reader, writer):
+async def client_handler(reader: StreamReader, writer: StreamWriter):
 
     peername: str = writer.get_extra_info("peername")
     logger.info(f"New client connected: {peername}")
-
     clients.add(writer)
 
+    try:
+        while True:
+            data = await reader.read(100)
+            if not data:
+                break
+    except:
+        pass
+    finally:
+        logger.info(f"Client {peername} disconnected")
+        if writer in clients:
+            clients.remove(writer)
+        writer.close()
+        await writer.wait_closed()
